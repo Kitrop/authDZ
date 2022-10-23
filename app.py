d556bdd6c998ef6d184f5b5ebd27ca1180ce18ea
Evgeniy_Shabolin <steamguardemail@yandex.ru>
2022-10-24 00:29:25 +0300

    add registration

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
 
 
@@ -9,36 +9,36 @@ conn = psycopg2.connect(database="auth_db",
                         user="postgres",
                         password="92657792Sha_",
                         host="localhost",
-
                         port="5432")
 
 cursor = conn.cursor()
 
-# if "".join(username.split()) != "" and "".join(password.split()) != "":
-#     error = ""
-# else:
-#     error = "не вверный логин или пароль"
 
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        login = request.form.get('login')
+        password = request.form.get('password')
 
+        cursor.execute('INSERT INTO auth.users (full_name, login, password) VALUES (%s, %s, %s);',
+                       (str(name), str(login), str(password)))
+        conn.commit()
 
+        return redirect('/login/')
+
+    return render_template('registration.html')
 
 
 @app.route('/login/', methods=['POST', 'GET'])
 def login():
-    error = ''
-    username = request.form.get('username')
-    password = request.form.get('password')
-
-
     if request.method == 'POST':
-        cursor.execute("SELECT * FROM auth.users WHERE login=%s AND password=%s", (str(username), str(password)))
-        records = list(cursor.fetchall())
-        return render_template('account.html', full_name=records[0][1], username=records[0][2], password=records[0][3])
-
-    if username == "" or password == "":
-        error = "не вверный логин или пароль"
-    else:
-        error = ""
-
-
-    return render_template('login.html', error = error)
+        if request.form.get("login"):
+            username = request.form.get('username')
+            password = request.form.get('password')
+            cursor.execute("SELECT * FROM auth.users WHERE login=%s AND password=%s", (str(username), str(password)))
+            records = list(cursor.fetchall())
+            return render_template('account.html', full_name=records[0][1])
+        elif request.form.get("registration"):
+            return redirect("/registration/")
+    return render_template('login.html')
